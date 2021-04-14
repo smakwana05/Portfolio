ef6352b99029aebd0f5269fb7511eea5cccff95c
surajmakwana <smakwana05@users.noreply.github.com>
2021-04-14 18:11:53 +0100

    111

--- Script.js
+++ Script.js
@@ -7,6 +7,51 @@ var menu = document.querySelector(".menu");
 var sticky = header.offsetTop;
 var gridwrapper = document.querySelector(".gridwrapper");
 var body = document.querySelector("body");
+var html = document.documentElement;
+
+const options = {
+  root: null,
+  rootMargin: '0px 0px 300px 0px',
+  threshold: 0.5
+}
+
+function test() {
+function imageLazyLoad() {
+  const images = Array.from(document.querySelectorAll('img[data-src]'));
+
+  if (images.length) {
+    if ('IntersectionObserver' in window) {
+      setupIntersectionObserver(images);
+    } else {
+      loadImages(images);
+    }
+  }
+}
+
+function setupIntersectionObserver(images) {
+  const observer = new IntersectionObserver(onIntersection, options);
+  images.forEach(image => observer.observe(image));
+}
+
+function onIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio >= 0.5) {
+      observer.unobserve(entry.target);
+      loadImage(entry.target);
+    }
+  });
+}
+
+function loadImages(images) {
+  images.forEach(loadImage);
+}
+
+function loadImage(image) {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => image.removeAttribute('data-src');
+}
+imageLazyLoad();
+}
 
 
 function goBack() {
@@ -36,6 +81,7 @@ function leaveAnimation() {
   })
  
 }
+
 
 
 barba.init({
@@ -115,7 +161,7 @@ name: 'masonry',
 
       enter(data) {
         setTimeout(function(){header.classList.add("shrink");}, 0);
-        
+
         gsap.set(data.next.container, 
           {autoAlpha: 0});
 
@@ -138,6 +184,8 @@ name: 'masonry',
 
 });
 
+
+
 if (history.scrollRestoration) {
   history.scrollRestoration = 'manual';
 }
@@ -149,7 +197,11 @@ barba.hooks.leave(() => {
 
 barba.hooks.enter(() => {
   window.scrollTo(0, 0);
+  test();
 });
 
+barba.hooks.once(() => {
+  test();
+});
 
-
+  
